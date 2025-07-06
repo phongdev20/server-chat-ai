360588aa9575bc056317375518821442012e36d7
tranphong <phongdevinfo@gmail.com>
2025-07-06 11:18:15 +0700

    [feat] update cron active server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const cron = require("./config/cron");
 
 const chatRoute = require("./routes/chat");
 const routes = require("./routes");
 
 const app = express();
+
+cron.start;
 
 app.use(cors());
 app.use(express.json());
